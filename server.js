06317a3ed3c60d7387f49b2c02884e3a22a81379
Amiir <amirlearn08@gmail.com>
2025-02-07 15:18:37 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotEnv = require("dotenv").config();
+const mongoose = require("mongoose");
+const app = express();
+app.use(express.json());
+app.use("/public/uploads", express.static("public/uploads"));
+const userRouter = require("./src/user/router");
+const categoryRouter = require("./src/category/router");
+const productRouter = require("./src/product/router");
+const paymentRouter = require("./src/payment/router");
+const ordersRouter = require("./src/orders/router");
+app.use("/user", userRouter);
+app.use("/category", categoryRouter);
+app.use("/product", productRouter);
+app.use("/payment", paymentRouter);
+app.use("/order", ordersRouter);
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log("DB connected ✅"))
+  .catch((err) => console.log("DB connection error ❌", err));
+app.listen(process.env.PORT, () =>
+  console.log(`PORT SUCCESS ✅ AT ${process.env.PORT}`)
+);
